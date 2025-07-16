7cca18d5e18b8d74432aa913892e8de53469dd6a
keshew <artyom.korotckov2017@yandex.ru>
2025-07-16 19:54:11 +0500

    Initial Commit

---
+++ Vulcan/VulcanApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulcanApp.swift
+//  Vulcan
+//
+//  Created by Артём Коротков on 16.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct VulcanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
